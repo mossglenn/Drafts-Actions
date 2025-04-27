dd5a4aaae75760da57d9e6625113683c92ae279e
Amos Glenn <amos@amosglenn.com>
2025-04-27 18:01:35 -0400

    split credentials from Customizable Values

--- Actions/frictionless-to-jira-script.js
+++ Actions/frictionless-to-jira-script.js
@@ -1,87 +1,65 @@
-// ===== USER SETTINGS =====
-const DEFAULT_ISSUE_TYPE = 'Task'; // Change to "Bug", "Story", etc.
-const INCLUDE_TAGS_AS_LABELS = true; // Set false to disable adding Draft tags as Jira labels
-const SENT_FROM_DRAFTS_LABEL = 'send-from-drafts'; // Change text or set to false to disable adding a "sent-from-drafts" label
-const TAG_AFTER_SEND = 'sent-to-jira'; // change text or set to false to disable adding a tag after sending
-const ARCHIVE_AFTER_SEND = true; // Set false to disable moving the Draft to Archive
+/* This script creates a new Jira issue using the Jira REST API. The new issue is created based on the current draft content.
+ * The script requires the following configured values:
+ * - jiraDomain: The Jira domain (e.g., yourcompany.atlassian.net)
+ * - jiraProjectKey: The Jira project key (e.g., PROJ)
+ * - jiraIssueType: The Jira issue type (e.g., Task, Bug)
+ * - includeTagsAsLabels: Whether to include draft tags as Jira labels (default: true)
+ * - includeJiraLabel: Whether to include a specific Jira label (default: true)
+ * - jiraLabel: The specific Jira label to include (default: 'sent-from-drafts')
+ * The script also requires a credential named CREDENTIAL_NAME, which can be reset below. The credential includes:
+ * - email: The email address of the Jira user
+ * - token: The API token for the Jira user
+ * After the new Jira issue is created, the script will prepend teh draft with the Markdown link to the new issue.
+ */
 
-const MAIN_CREDENTIAL_NAME = 'jira-basic-auth'; //name of credential with URL, email, and token
-const ALT_CREDENTIAL_NAME = 'jira-domain-and-project'; //used only when URL cannot be parsed
+// ===== User Settings =====
+const CREDENTIAL_NAME = 'frictionless-to-jira'; //name of credential with email, and token
+// ========================
 
-// =========================
+const settings = getConfiguredValues();
 
-// ===== Future Possibilities ===== //
-/* 
--Multi-level headings (##, ###)
--grouped bullet/numbered lists
--block quotes
--basic rich text formatting 
-*/
-//===================
-
-// collect JIRA API settings and construct API endpoint
-let jira = getJiraSettings();
-const apiURL = `https://${jira.domain}/rest/api/3/issue`;
-
+const jira = {};
 // Use current draft content to construct Jira Issue fields
-let lines = draft.content.trim().split('\n');
-
-// The first line is the summary (or title) of the JIRA ticket
+const lines = draft.content.trim().split('\n');
 jira.summary = lines[0] || 'Untitled';
+jira.description = makeAdfDescription(lines.slice(1));
+jira.labels = getJiraLabels(draft.tags);
 
-// Remaining lines are used to construct the Atlassian Document Format (ADF) for the description
-// TODO: use Atlassian library to better include emoji, unicode, etc.
-const descriptionBody = lines.slice(1).join('\n') || 'No description.';
-jira.description = {
-  type: 'doc',
-  version: 1,
-  content: [
-    {
-      type: 'paragraph',
-      content: [
-        {
-          type: 'text',
-          text: descriptionBody,
-        },
-      ],
-    },
-  ],
-};
-
-// Construct the fields object for the JIRA API request
-let payloadBody = {
+const req = {};
+//construct request values
+req.url = `https://${settings.domain}/rest/api/3/issue`;
+req.cred = getCredentials();
+req.payloadBody = {
   fields: {
-    project: { key: jira.projectKey },
-    issuetype: { name: DEFAULT_ISSUE_TYPE },
+    project: { key: settings.projectKey },
+    issuetype: { name: settings.issueType },
     summary: jira.summary,
     description: jira.description,
+    labels: jira.labels,
   },
 };
-jira.labels = getJiraLabels();
-if (jira.labels) {
-  payloadBody.fields.labels = jira.labels;
-}
 
 // Make headers for Basic auth
-let authString = `${jira.email}:${jira.token}`;
-let authCode = 'Basic ' + Base64.encode(authString);
-let apiHeaders = {
-  Authorization: authCode,
+req.headers = {
+  Authorization:
+    'Basic ' + Base64.encode(`${req.cred.email}:${req.cred.token}`),
   Accept: 'application/json',
   'Content-Type': 'application/json',
 };
 
-//see payload in consol for debugging
+//see request in consol for debugging
+console.log('Request headers:');
+console.log(JSON.stringify(req.headers, null, 2));
 console.log('Request body:');
 console.log(JSON.stringify(payloadBody, null, 2));
 
 // Make a POST request to the JIRA API to create a new issue
-let http = HTTP.create();
-let response = http.request({
+const http = HTTP.create();
+const response = http.request({
   method: 'POST',
-  url: apiURL,
-  headers: apiHeaders,
-  data: payloadBody,
+  url: req.url,
+  headers: req.headers,
+  data: req.payloadBody,
   timeout: 30.0,
 });
 
@@ -92,127 +70,108 @@ console.log(
 
 // Handle response
 if (response.statusCode === 403) {
-  context.fail('Access denied. Check your Jira permissions');
+  context.fail(
+    'Access denied. Check your Jira permissions. Forget credentials and rerun.',
+  );
 } else if (response.statusCode === 400) {
   context.fail('Bad request. Check project key or required fields.');
 } else if (response.success && response.statusCode == 201) {
-  let r = JSON.parse(response.responseText);
-  let ticketURL = `https://${jira.domain}/browse/${r.key}`;
+  const r = JSON.parse(response.responseText);
 
-  // Update draft with the created ticket URL
-  let t = `${r.key} // ${title} // ${ticketURL}`;
-  draft.content = t + '\n\n' + draft.content;
+  // Update draft with the created issue URL
+  draft.prepend(`Jira Key: (${r.key})[${r.self}]`, '\n\n');
   draft.update();
 
-  // Apply sent tag if enabled
-  if (TAG_AFTER_SEND) {
-    draft.addTag(TAG_AFTER_SEND);
-    draft.update();
-  }
-
-  // Move to archive if enabled
-  if (ARCHIVE_AFTER_SEND) {
-    draft.isArchived = true;
-    draft.update();
-  }
-
   app.displaySuccessMessage('Created ' + r.key + ' in JIRA');
-  app.setClipboard(ticketURL);
+  app.setClipboard(`${r.self}`);
 } else {
   app.displayErrorMessage(
     'Failed to create JIRA ticket. Check the console for details.',
   );
   context.fail('ERROR ' + response.statusCode + ' ' + response.responseText);
 }
-console.log(
-  `Reminder: If you have changed Jira servers, you will need forget TWO credentials: go to Drafts Preferences > Credentials and select forget next to credentials named ${MAIN_CREDENTIAL_NAME} and ${ALT_CREDENTIAL_NAME}.`,
-);
 
 //====== functions ======== //
 
-//collect JIRA API values from credentials (two if necessary)
-function getJiraSettings() {
-  let mainCred = getMainCredentials();
-  let settings = {
-    email: mainCred.getValue('email'),
-    token: mainCred.getValue('token'),
-    url: mainCred.getValue('projectURL'),
-  };
+//get Configured Values
+function getConfiguredValues() {
+  const s = {};
+  s.jiraDomain = context.getConfiguredValue('jiraDomain')
+    ? context.getConfiguredValue('jiraDomain')
+    : null;
+  s.jiraProjectKey = context.getConfiguredValue('jiraProjectKey')
+    ? context.getConfiguredValue('jiraProjectKey')
+    : null;
+  s.jiraIssueType = context.getConfiguredValue('jiraIssueType')
+    ? context.getConfiguredValue('jiraIssueType')
+    : 'Task';
+  s.includeTagsAsLabels = context.getConfiguredValue('includeTagsAsLabels')
+    ? context.getConfiguredValue('includeTagsAsLabels')
+    : true;
+  s.includeJiraLabel = context.getConfiguredValue('includeJiraLabel')
+    ? context.getConfiguredValue('includeJiraLabel')
+    : true;
+  s.jiraLabel = context.getConfiguredValue('jiraLabel')
+    ? context.getConfiguredValue('jiraLabel')
+    : 'sent-from-drafts';
 
-  let parsed = parseJiraProjectURL(settings.url);
-  if (parsed) {
-    settings.domain = parsed.domain;
-    settings.projectKey = parsed.projectKey;
+  if (!s.jiraDomain || !s.jiraProjectKey) {
+    context.fail('Missing JIRA domain or project key.');
+  }
+  return s;
+}
+// credentials user email, and Atlassian token
+function getCredentials() {
+  const c = Credential.create(
+    CREDENTIAL_NAME,
+    'JIRA credentials for ad hoc API calls using an email, and a token',
+  );
+  c.addTextField('email', 'Email Address');
+  c.addPasswordField('token', 'API Token');
+  c.authorize(); // This will prompt if not already saved
+  if (c) {
+    return {
+      email: c.getValue('email'),
+      token: c.getValue('token'),
+    };
   } else {
-    let altCred = getAltCredentials();
-    settings.domain = altCred.getValue('domain');
-    settings.projectKey = altCred.getValue('projectKey');
+    context.fail('Failed to get credentials');
   }
-
-  if (
-    !settings.domain ||
-    !settings.projectKey ||
-    !settings.email ||
-    !settings.token
-  ) {
-    console.log('Something went wrong:', JSON.stringify(settings, null, 2));
-    context.fail('Incomplete Jira settings.');
-  }
-
-  return settings;
 }
 
-//parse url to get JIRA domain and Project Key
-function parseJiraProjectURL(url) {
-  let matches = url.match(
-    /^https:\/\/([^.]+\.atlassian\.net)\/jira\/[^\/]+\/projects\/([A-Z0-9]+)\b/i,
-  );
-  if (!matches) return null;
-
+//turn text lines into jira description with Atlassian Document Format (ADF)
+function makeAdfDescription(lines) {
+  const descriptionBody = lines.slice(1).join('\n') || 'No description.';
   return {
-    domain: matches[1],
-    projectKey: matches[2],
+    type: 'doc',
+    version: 1,
+    content: [
+      {
+        type: 'paragraph',
+        content: [
+          {
+            type: 'text',
+            text: descriptionBody,
+          },
+        ],
+      },
+    ],
   };
 }
 
-// main credentials include JIRA project url, user email, and Atlassian token
-function getMainCredentials() {
-  let main_c = Credential.create(
-    MAIN_CREDENTIAL_NAME,
-    'JIRA credentials for ad hoc API calls using a URL, an email, and a token',
-  );
-  main_c.addURLField('projectURL', 'Jira Project URL');
-  main_c.addTextField('email', 'Email Address');
-  main_c.addPasswordField('token', 'API Token');
-  main_c.authorize(); // This will prompt if not already saved
-  return main_c;
-}
-
-// alternative credentials include JIRA Cloud domain and Project Key; used only when url parsing fails
-function getAltCredentials() {
-  let alt_c = Credential.create(
-    ALT_CREDENTIAL_NAME,
-    'JIRA credentials for ad hoc API calls using a domain and a project key',
-  );
-  alt_c.addTextField('domain', 'Jira Domain (e.g., example.atlassian.net)');
-  alt_c.addTextField('projectKey', 'Jira Project Key (e.g., MKY)');
-  alt_c.authorize(); // This will prompt if not already saved
-  return alt_c;
-}
-
-function getJiraLabels() {
+// if set, convert draft tags to Jira-safe labels and add Jira label
+function getJiraLabels(draftTags) {
   let labels = [];
-  // Convert draft tags to Jira-safe labels (skipping internal use tag)
-  if (INCLUDE_TAGS_AS_LABELS) {
-    let tags = draft.tags;
-    let tagLabels = tags.filter((t) => t !== 'send-to-jira');
-    tagLabels = tagLabels.map((l) => l.toLowerCase().replace(/\s+/g, '-'));
+  if (settings.includeTagsAsLabels) {
+    const tagLabels = draftTags.map((l) =>
+      l.toLowerCase().replace(/\s+/g, '-'),
+    );
     labels = labels.concat(tagLabels);
   }
 
-  if (SENT_FROM_DRAFTS_LABEL) {
+  if (settings.includeJiraLabel) {
     labels = labels.concat(
-      SENT_FROM_DRAFTS_LABEL.toLowerCase().replace(/\s+/g, '-'),
+      settings.jiraLabel.toLowerCase().replace(/\s+/g, '-'),
     );
   }
   return labels;
--- README.md
+++ README.md
@@ -13,8 +13,7 @@
 - 📄 **Remaining lines** = Jira Description (formatted in Atlassian Document Format [ADF])
 - 🏷️ **Optional**: Draft tags can be automatically added as Jira labels
 - 🚀 **Fast and frictionless**: No formatting required — just type and send (bonus points for adding a ⏩️ keyboard shortcut)
-- 🔒 **Secure**: Uses Drafts’ Credential system to store your Jira API token securely
-- 🧼 **Self-Cleaning**: Add a tag to the Draft and archive (customizable)
+- 🔒 **Secure**: Uses Drafts’ Credential system to store your Atlassian username and API token securely
 
 ---
 
@@ -29,16 +28,14 @@
 ## 🔧 Setup Instructions
 
 1. **Install the Action** into Drafts.
-2. **First run**:  
+2. **Complete Required Settings**:
+   - The Jira Cloud domain (e.g., 'yourdomain.atlassian.net')
+   - The Jira Project Key (e.g., 'YJK')
+3. **Update Optional Settings**
+4. **First run**:  
    The Action will prompt you to securely enter your:
-   - Jira Project URL (e.g., `https://yourdomain.atlassian.net/jira/core/projects/KEY/summary`)
    - Jira Email Address
    - Jira API Token
-3. **Optional fallback**:  
-   If your Project URL cannot be parsed automatically, it will prompt for:
-   - Jira Domain (e.g., `yourdomain.atlassian.net`)
-   - Jira Project Key (e.g., `AG`)
-4. **Save credentials** when prompted.  
    Drafts will remember them securely for future use.
 5. **Create a Draft** with the content you want.
 6. **Run the Action** (keyboard shortcut recommended).
@@ -50,57 +47,41 @@
 - Converts remaining lines into a plain text  **Description**:
 - Adds optional labels:
   - **Draft tags**
-  - **Custom "sent-from-drafts" label** (configurable)
-- Updates the Draft: (configurable)
-   - **Adds a link** to the newly created Jira issue
-   - **Add 'sent-to-jira' tag**
-   - **Archive Draft**
-
+  - **Customizable Jira label**
+- **Adds link to new Jira issue at the top of the Draft.
 
 
 ## ⚙️ Configuration
 
-You can adjust the Action’s behavior by editing the **user settings** at the top of the script:
+You can adjust the Action’s behavior by editing the following **settings**:
+| Setting | instructions |
+|---|---|
+| Issue Type | The name of any *Task-level* type of issue existing in the target Jira project (for example: `'Bug'`, `'Story'` or a custom type like `'Drafts'` ) <p><p>\[Note: types like `'Epic'` and `'Subtask'` are *untested*\] |
+| Include Tags as Labels | All tags on the current draft are included in the new Jira issue as labels. Draft tags are made Jira-safe with conversion to lowercase with no spaces (for example: 'My Tag' becomes 'my-tag'). <p><p>Even when this setting is deactivated, you can **still include the label** from 'Custom Jira Label' setting. |
+| Include Jira Label | Add a label to the new Jira issue's labels. Label is taken from the 'Custom Jira Label' setting. |
+| Custom Jira Label | Any **Jira-safe** label text added to the new Jira issue's labels. To not include this label, deactivate the 'Include Jira Label' setting. This setting does not interact with the 'Include Tags as Labels' setting. |
 
+#### Credential Name
+Inside the script, you can configure the name of the Credential containing your email and token by changing the CREDENTIAL_NAME constant. The default name is 'frictionless-to-Jira'. 
 ```javascript
 // ===== USER SETTINGS =====
-const DEFAULT_ISSUE_TYPE = 'Task'; // Change to "Bug", "Story", etc.
-const INCLUDE_TAGS_AS_LABELS = true; // Set false to disable adding Draft tags as Jira labels
-const SENT_FROM_DRAFTS_LABEL = 'send-from-drafts'; // Change text or set to false to disable adding a "sent-from-drafts" label
-const TAG_AFTER_SEND = 'sent-to-jira'; // change text or set to false to disable adding a tag after sending
-const ARCHIVE_AFTER_SEND = true; // Set false to disable moving the Draft to Archive
+const CREDENTIAL_NAME = 'frictionless-to-jira'; //name of credential with email, and token
 ```
-
-| CONST | instructions |
-|---|---|
-| DEFAULT_ISSUE_TYPE | Set to the `string` name of any Task-level type of issue existing in the target Jira project (for example: `'Bug'`, `'Story'` ) \[Note: `'Epic'` and `'Subtask'` are *untested*\] |
-| INCLUDE_TAGS_AS_LABELS | Set to `true` to copy all the tags in the Draft into the new Jira issue. Draft tags are converted to lowercase with no spaces to make them Jira-safe (for example: 'My Tag' becomes 'my-tag'). <br />Set to `false` to not include Draft tags as Jira labels, but you can **still include the label set in SENT_FROM_DRAFTS_LABEL.** |
-| SENT_FROM_DRAFTS_LABEL | Set to any **Jira-safe** `string` to include that as a label in the new Jira issue **even if INCLUDE_TAGS_AS_LABELS is `false`**. <br /> Set to `false` to not include this label, but **does not prevent Draft tags from being included** in the new Jira issue with INCLUDE_TAGS_AS_LABELS.|
-| TAG_AFTER_SEND | Set to any Draft-safe `string` to add that tag to the Draft. <br /> Set to `false` to not add any new tag to the Draft. |
-| ARCHIVE_AFTER_SEND | Set to `true` to automatically send the Draft to the Drafts Archive as soon as the new Jira issue is created. <br />Set to `false` to keep the Draft where it is.|
-| MAIN_CREDENTIAL_NAME<br />ALT_CREDENTIAL_NAME | Set to any `string` to name the credentials set for this Action. Included in case of conflicts with other Drafts credentials.<br />ALT_CREDENTIAL_NAME is only used when necessary, so it may not appear in your Drafts Credentials list.<br >**If you need to forget credentials for this Action, make sure to look for BOTH credentials to forget.**
-
-
 ---
-
 
 ## 🛡️ Securing Secret Information
 
-When communication with Jira Cloud, Frictionless-to-Jira collects *some or all* of the following information:
-- **URL** of the target Jira project
+When communication with Jira Cloud, Frictionless-to-Jira collects:
 - **Email** of Atlassian user
 - **Token** of Atlassian user
-- **Domain** of your Jira Cloud
-- **Project Key** of the target Jira project
 
 This information is stored securely in Drafts Credentials. Once saved, you don't have to enter it again unless you manually forget the Credential.
 
-## ☠️ Removing and Forgetting Secret Information
-Drafts does not let you view or edit saved Credentials directly. To update your Jira credentials:
+#### Removing or Changing Credentials
+Drafts does not let you view or edit saved Credentials directly. To update your Atlassian credentials:
 1) Open Drafts Preferences > Credentials.
-2) Look for the Credential named 'jira-basic-auth' (or your customized MAIN_CREDENTIAL_NAME).
+2) Look for the Credential named 'frictionless-to-jira' (or your customized CREDENTIAL_NAME).
 3) Click `forget` to delete it.
-4) Look for the Credential named 'jira-domain-and-project' (or your ALT_CREDENTIAL_NAME) and forget it **if it exists**. Frictionless-to-Jira doesn't create this second credential often, but check for it. 
 
 Drafts will ask you for new information the next time the Action runs.
 
